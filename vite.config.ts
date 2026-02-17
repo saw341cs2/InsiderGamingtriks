2109284fc501c6adacfefa73bdb294113d53a7d4
Saw341 <saw341cs2@gmail.com>
2026-02-17 18:06:17 +0100

    Fix: relative paths for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/InsiderGamingtriks/',
+  base: './',
   build: {
     outDir: 'docs',
   },
