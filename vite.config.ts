3ed9c86d74278a1345f2d1d13e6fb63d9ff8be3d
Saw341 <saw341cs2@gmail.com>
2026-02-22 19:39:52 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,29 +2,23 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: "/InsiderGamingtriks/",
+
   plugins: [react()],
-})
-  base: './',
+
   build: {
-    outDir: 'docs',
+    outDir: "docs",
   },
+
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react()
-  ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
-      export default defineConfig({
-  base: "/InsiderGamingtriks/",
-  plugins: [react()],
-})
+});
