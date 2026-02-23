43a0c82f61537982ce39a8b2c0d91bb3b467f4dc
Saw341 <saw341cs2@gmail.com>
2026-02-23 04:52:14 +0100

    Fix: Remove ProtectedRoute qui causait écran noir

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,12 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AppLayout from './components/AppLayout';
 import Index from './pages/Index';
 import NotFound from './pages/NotFound';
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
     <BrowserRouter basename="/InsiderGamingtriks/">
       <Routes>
-        <Route path="/" element={
-          <ProtectedRoute>
-            <AppLayout />
-          </ProtectedRoute>
-        }>
+        <Route path="/" element={<AppLayout />}>
           <Route index element={<Index />} />
           <Route path="*" element={<NotFound />} />
         </Route>
