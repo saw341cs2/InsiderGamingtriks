be67e9dd1a55b01dab78daa187700b5fad11634e
Saw341 <saw341cs2@gmail.com>
2026-03-10 23:15:55 +0100

    Add HashRouter for GitHub Pages compatibility

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
 import Index from './pages/Index.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(<Index />);
+createRoot(document.getElementById("root")!).render(<HashRouter><Index /></HashRouter>);
