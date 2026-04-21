3ca35269285252826d76824f1f2af38e43c2425f
Saw341 <saw341cs2@gmail.com>
2026-04-21 14:32:49 +0200

    Updates

--- src/lib/supabase.ts
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = 'https://tpyvpfjertxklktxlmux.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRweXZwZmplcnR4a2xrdHhsbXV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU4OTYzOTAsImV4cCI6MjA5MTQ3MjM5MH0.J6IWSYrmH2IJ8Hrjd5hgICqGiM39lwgOz8uZE8sKyGw';
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
