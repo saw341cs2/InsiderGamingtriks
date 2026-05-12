f246dc389e682c08f7c4af42f343699973f3e9bc
Saw341 <saw341cs2@gmail.com>
2026-05-12 19:33:25 +0200

    fix: correction clé supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 ﻿import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://tpyvpfjertxklktxlmux.supabase.co';
-const supabaseAnonKey = 'sb_publishable_bCpIi7bK0Zw68c69AOGAkQ_cojChbRL';
+const supabaseUrl = 'https://dsohltltdfobdnldlihtw.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRzb2hsdGx0ZGZvYm5sZGxpaHR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4MDg1MzksImV4cCI6MjA4NzM4NDUzOX0.cnjw3RSXhwqGNRI2zh-huFXDULa_vBHxSVDkfDu7GfQ';
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
