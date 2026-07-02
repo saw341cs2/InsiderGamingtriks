5f16c2eba84be3eb2f8196d4c43b2eef1f82f281
Saw341 <saw341cs2@gmail.com>
2026-07-02 12:01:06 +0200

    AppProvider manquant, corrige le spinner infini

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
 import App from "./App"
+import { AppProvider } from "@/contexts/AppContext"
 import "./index.css"
-
-console.log("UNIQUEMAIN12345 BUILD WORKS")
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AppProvider>
+        <App />
+      </AppProvider>
     </BrowserRouter>
   </React.StrictMode>,
 )
