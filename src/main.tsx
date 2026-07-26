d46fe613ac58b9da025c19efe1f70509291122f8
Saw341 <saw341cs2@gmail.com>
2026-07-26 18:33:33 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
-import App from "./App"
-import { AppProvider } from "@/contexts/AppContext"
-import "./index.css"
+// src/main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { HelmetProvider } from 'react-helmet-async';  // ← AJOUTE CETTE LIGNE
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AppProvider>
-        <App />
-      </AppProvider>
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <HelmetProvider>    {/* ← AJOUTE CETTE BALISE OUVRANTE */}
+    <App />
+  </HelmetProvider>   {/* ← AJOUTE CETTE BALISE FERMANTE */}
+);
